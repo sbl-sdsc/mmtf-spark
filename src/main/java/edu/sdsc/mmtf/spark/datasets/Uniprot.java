6000d1efa49533ed899191db1fa5cdd70c15296d
yuy079 <yuy079@ucsd.edu>
2017-08-02 14:54:09 -0700

    uniprot done

--- src/main/java/edu/sdsc/mmtf/spark/datasets/Uniprot.java
+++ src/main/java/edu/sdsc/mmtf/spark/datasets/Uniprot.java
@@ -1,9 +1,11 @@
 package edu.sdsc.mmtf.spark.datasets;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
 
 import edu.sdsc.mmtf.spark.ml.JavaRDDToDataset;
 
@@ -23,161 +26,170 @@ import edu.sdsc.mmtf.spark.ml.JavaRDDToDataset;
  * 
  */
 public class Uniprot {
-	private static final String Swiss_Prot_URL = "ftp://ftp.uniprot.org/pub/databases/"
-			+ "uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz";
-	private static final String TreMBL_URL = "ftp://ftp.uniprot.org/pub/databases/"
-			+ "uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz";
-	private static final String UniRef50_URL = "ftp://ftp.uniprot.org/pub/databases/"
-			+ "uniprot/uniref/uniref50/uniref50.fasta.gz";
-	
-	/**
-	 * TODO
-	 */
-	public static Dataset<Row> getDataset(JavaSparkContext sc, String dataType) throws IOException {	
-		URL u = null;
-		if(dataType.equals("swiss_prot"))
+	public enum UniDataset{
+		SWISS_PROT("ftp://ftp.uniprot.org/pub/databases/"
+				+ "uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz"),
+		TREMBL("ftp://ftp.uniprot.org/pub/databases/"
+				+ "uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz"),
+		UNIREF50("ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref50/uniref50.fasta.gz"),
+		UNIREF90("ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz"),
+		UNIREF100("ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref100/uniref100.fasta.gz");
+		private final String url;
+		UniDataset(String url)
 		{
-			u = new URL(Swiss_Prot_URL);
+			this.url = url;
 		}
-		if(dataType.equals("TreMBL"))
+		public String getUrl()
 		{
-			u = new URL(TreMBL_URL);
-		}
-		if(u != null)
-		{
-			URLConnection conn = u.openConnection();
-			List<Row> res = new ArrayList<Row>();
-			InputStream in = conn.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(new GZIPInputStream(in)));
-			
-			String db = "", uniqueIdentifier= "", entryName= "", proteinName= "", organismName= "", geneName= "";
-			int proteinExistence=0, sequenceVersion=0;
-			String sequence = "";
-			String line;
-			String[] tmp;
-			boolean firstTime = true;
-			while((line = rd.readLine()) != null)
-			{
-				if(line.contains(">"))
-				{
-					//System.out.println(line);
-					if(!firstTime)
-						res.add(RowFactory.create(db, uniqueIdentifier, entryName, proteinName, organismName, geneName, proteinExistence, sequenceVersion, sequence));
-					firstTime = false;
-					sequence = "";
-					tmp = (line.substring(1)).split("\\|");
-					db = tmp[0];
-					uniqueIdentifier = tmp[1];
-					tmp[0] = tmp[2];
-					if(tmp[0].indexOf(" SV=") != -1)
-					{
-						tmp = tmp[0].split(" SV=");
-						sequenceVersion = Integer.parseInt(tmp[1]);
-					}
-					else sequenceVersion = -1;
-					if(tmp[0].indexOf(" PE=") != -1)
-					{
-						tmp = tmp[0].split(" PE=");
-						proteinExistence = Integer.parseInt(tmp[1]);
-					}
-					else proteinExistence = -1;				
-					if(tmp[0].indexOf(" GN=") != -1)
-					{
-						tmp = tmp[0].split(" GN=");
-						geneName = tmp[1];
-					}
-					else geneName = "null";	
-					if(tmp[0].indexOf(" OS=") != -1)
-					{
-						tmp = tmp[0].split(" OS=");
-						organismName = tmp[1];
-					}
-					else organismName = "null";					
-					
-					entryName = tmp[0].split(" ")[0];
-					proteinName = tmp[0].substring(tmp[0].split(" ")[0].length() + 1);
-	//				
-	//				System.out.println("db is : " + db + "\nUniqueId is : " + uniqueIdentifier
-	//						+ "\nEntryName is : " + entryName + "\nproteinName is : " + proteinName
-	//						+ "\norganismName is : " + organismName + "\ngeneName is : " + geneName
-	//						+ "\nproteinExistence is : " + proteinExistence + "\nsequenceVersion is : " + sequenceVersion);
-				}
-				else{
-					sequence = sequence.concat(line);
-				}
-			}
-			
-			
-			JavaRDD<Row> data =  sc.parallelize(res);
-			
-			return JavaRDDToDataset.getDataset(data, "db", "uniqueIdentifier", "entryName",
-					"proteinName", "organismName", "geneName", "proteinExistence", "sequenceVersion", "sequence");
-		}
-		//Uniref
-		else
-		{
-			if(dataType.equals("UniRef50"))
-				u = new URL(UniRef50_URL);
-			URLConnection conn = u.openConnection();
-			List<Row> res = new ArrayList<Row>();
-			InputStream in = conn.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(new GZIPInputStream(in)));
-			String line;
-			
-			String uniqueIdentifier = "", clusterName = "", taxon = "", representativeMember = "";
-			int taxonID = -1, members = -1;
-			boolean firstTime = true;
-			String sequence = "";
-			String[] tmp = new String[1];
-			while((line = rd.readLine()) != null)
-			{
-				if(line.contains(">"))
-				{
-					System.out.println(line);
-					if(!firstTime)
-						res.add(RowFactory.create(uniqueIdentifier, clusterName, members, taxon, taxonID, representativeMember, sequence));
-					firstTime = false;
-					sequence = "";
-					tmp[0] = line.substring(1);
-					if(tmp[0].indexOf(" RepID=") != -1)
-					{
-						tmp = tmp[0].split(" RepID=");
-						representativeMember = tmp[1];
-					}
-					else representativeMember = "null";
-					if(tmp[0].indexOf(" TaxID=") != -1)
-					{
-						tmp = tmp[0].split(" TaxID=");
-						taxonID = Integer.parseInt(tmp[1]);
-					}
-					else taxonID = -1;	
-					if(tmp[0].indexOf(" Tax=") != -1)
-					{
-						tmp = tmp[0].split(" Tax=");
-						taxon = tmp[1];
-					}
-					else taxon = "null";
-				
-					if(tmp[0].indexOf(" n=") != -1)
-					{
-						
-						tmp = tmp[0].split(" n=");
-						members = Integer.parseInt(tmp[1]);
-					}
-					else members = -1;					
-					
-					uniqueIdentifier = tmp[0].split(" ")[0];
-					clusterName = tmp[0].substring(tmp[0].split(" ")[0].length() + 1);
-				}
-				else{
-					sequence = sequence.concat(line);
-				}
-			}
-			JavaRDD<Row> data = null;
-			
-			return JavaRDDToDataset.getDataset(data, "uniqueIdentifier", "clusterName", "members",
-					"taxon", "taxonID", "representativeMember", "sequence");
+			return url;
 		}
 	}
-	
+	/**
+	 * 
+	 */
+	public static Dataset<Row> getDataset(JavaSparkContext sc, UniDataset dataType) throws IOException {		
+		switch(dataType){
+			case SWISS_PROT: return getUniprotDataset(sc, dataType);
+			case TREMBL: return getUniprotDataset(sc, dataType);
+			case UNIREF50: return getUnirefDataset(sc, dataType);
+			case UNIREF90: return getUnirefDataset(sc, dataType);
+			case UNIREF100: return getUnirefDataset(sc, dataType);
+			default: throw new IOException("unknown dataType: " + dataType);
+		}
+	}
+	private static Dataset<Row> getUniprotDataset(JavaSparkContext sc, UniDataset dataType) throws IOException {
+		URL u = new URL(dataType.getUrl());
+		URLConnection conn = u.openConnection();
+		List<Row> res = new ArrayList<Row>();
+		InputStream in = conn.getInputStream();
+		BufferedReader rd = new BufferedReader(new InputStreamReader(new GZIPInputStream(in)));
+		
+		String db = "", uniqueIdentifier= "", entryName= "", proteinName= "", organismName= "", geneName= "";
+		int proteinExistence=0, sequenceVersion=0;
+		String sequence = "", line;
+		String[] tmp;
+		boolean firstTime = true;
+		while((line = rd.readLine()) != null)
+		{
+			if(line.contains(">"))
+			{
+				//System.out.println(line);
+				if(!firstTime)
+					res.add(RowFactory.create(db, uniqueIdentifier, entryName, proteinName,
+							organismName, geneName, proteinExistence, sequenceVersion, sequence));
+				firstTime = false;
+				sequence = "";
+				tmp = (line.substring(1)).split("\\|");
+				db = tmp[0];
+				uniqueIdentifier = tmp[1];
+				tmp[0] = tmp[2];
+				if(tmp[0].indexOf(" SV=") != -1)
+				{
+					tmp = tmp[0].split(" SV=");
+					sequenceVersion = Integer.parseInt(tmp[1]);
+				}
+				else sequenceVersion = -1;
+				if(tmp[0].indexOf(" PE=") != -1)
+				{
+					tmp = tmp[0].split(" PE=");
+					proteinExistence = Integer.parseInt(tmp[1]);
+				}
+				else proteinExistence = -1;				
+				if(tmp[0].indexOf(" GN=") != -1)
+				{
+					tmp = tmp[0].split(" GN=");
+					geneName = tmp[1];
+				}
+				else geneName = "";	
+				if(tmp[0].indexOf(" OS=") != -1)
+				{
+					tmp = tmp[0].split(" OS=");
+					organismName = tmp[1];
+				}
+				else organismName = "";					
+				entryName = tmp[0].split(" ")[0];
+				proteinName = tmp[0].substring(tmp[0].split(" ")[0].length() + 1);
+			}
+			else{
+				sequence = sequence.concat(line);
+			}
+		}
+		res.add(RowFactory.create(db, uniqueIdentifier, entryName, proteinName,
+				organismName, geneName, proteinExistence, sequenceVersion, sequence));
+		JavaRDD<Row> data =  sc.parallelize(res);
+		return JavaRDDToDataset.getDataset(data, "db", "uniqueIdentifier", "entryName",
+				"proteinName", "organismName", "geneName", "proteinExistence", "sequenceVersion", "sequence");
+	}
+	private static Dataset<Row> getUnirefDataset(JavaSparkContext sc, UniDataset dataType) throws IOException {
+		File tempFile = File.createTempFile("tmp", ".csv");
+		PrintWriter pw = new PrintWriter(tempFile);
+		pw.println("uniqueIdentifier,clusterName,members,taxon,taxonID,representativeMember,sequence");
+		
+		
+		URL	u = new URL(dataType.getUrl());
+		URLConnection conn = u.openConnection();
+		InputStream in = conn.getInputStream();
+		BufferedReader rd = new BufferedReader(new InputStreamReader(new GZIPInputStream(in)));
+		String line, sequence = "";
+		String uniqueIdentifier = "", clusterName = "", taxon = "", representativeMember = "", taxonID = "", members = "";
+		boolean firstTime = true;
+		String[] tmp = new String[1];
+		while((line = rd.readLine()) != null)
+		{
+			if(line.contains(">"))
+			{
+				//if(uniqueIdentifier.equals("UniRef50_Q6GZW3")) break;
+				//System.out.println(line);
+				if(!firstTime)
+					pw.println(uniqueIdentifier + "," + clusterName + "," + members + "," + taxon
+							 + "," + taxonID + "," +  representativeMember + "," +  sequence);
+				firstTime = false;
+				sequence = "";
+				tmp[0] = line.substring(1);
+				if(tmp[0].split(" RepID=").length > 1)
+				{
+					tmp = tmp[0].split(" RepID=");
+					representativeMember = tmp[1];
+				}
+				else representativeMember = "";
+				if(tmp[0].split(" TaxID=").length > 1)
+				{
+					tmp = tmp[0].split(" TaxID=");
+					taxonID = tmp[1];
+				}
+				else taxonID = "";	
+				if(tmp[0].split(" Tax=").length > 1)
+				{
+					tmp = tmp[0].split(" Tax=");
+					taxon = tmp[1];
+				}
+				else taxon = "";
+				if(tmp[0].split(" n=").length > 1)
+				{
+					
+					tmp = tmp[0].split(" n=");
+					members = tmp[1];
+				}
+				else members = "";					
+				uniqueIdentifier = tmp[0].split(" ")[0];
+				clusterName = tmp[0].substring(tmp[0].split(" ")[0].length() + 1);
+			}
+			else{
+				sequence = sequence.concat(line);
+			}
+		}
+		pw.println(uniqueIdentifier + "," + clusterName + "," + members + "," + taxon
+				 + "," + taxonID + "," +  representativeMember + "," +  sequence);
+		pw.close();
+		SparkSession spark = SparkSession
+	    		.builder()
+	    		.getOrCreate();
+		Dataset<Row> dataset = spark.read()
+	    		.format("csv")
+	    		.option("header", "true")
+	    		.option("inferSchema", "true")
+	    		.load(tempFile.toString());
+		return dataset;
+		
+	}
 }
--- src/main/java/edu/sdsc/mmtf/spark/datasets/demos/UniProtDemo.java
+++ src/main/java/edu/sdsc/mmtf/spark/datasets/demos/UniProtDemo.java
@@ -8,6 +8,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 import edu.sdsc.mmtf.spark.datasets.Uniprot;
+import edu.sdsc.mmtf.spark.datasets.Uniprot.UniDataset;
 
 public class UniProtDemo {
 	public static void main(String[] args) throws IOException {    
@@ -15,12 +16,12 @@ public class UniProtDemo {
 	    
 	    SparkConf conf = new SparkConf().setMaster("local[*]").setAppName(CustomReportDemo.class.getSimpleName());
 	    JavaSparkContext sc = new JavaSparkContext(conf);
-	    Dataset<Row> ds = Uniprot.getDataset(sc, "UniRef50");
+	    Dataset<Row> ds = Uniprot.getDataset(sc, UniDataset.UNIREF100);
 	    
 	    // show the schema of this dataset
 	    ds.printSchema();
-	    ds.show(200, false);
-
+	    ds.show(20, false);
+	    System.out.println("Count: " + ds.count());
 	    long end = System.nanoTime();
 	    
 	    System.out.println("Time:     " + (end-start)/1E9 + "sec.");
