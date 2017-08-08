3c6ad8aae959c8433b2a70079b3baa92b26a57cf
yuy079 <yuy079@ucsd.edu>
2017-08-08 12:23:24 -0700

    StructureToProteinDimers begins

---
+++ src/main/java/edu/sdsc/mmtf/spark/mappers/demos/MapToProteinDimers.java
@@ -0,0 +1,45 @@
+package edu.sdsc.mmtf.spark.mappers.demos;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.rcsb.mmtf.api.StructureDataInterface;
+
+import edu.sdsc.mmtf.spark.datasets.demos.CustomReportDemo;
+import edu.sdsc.mmtf.spark.io.MmtfReader;
+import edu.sdsc.mmtf.spark.io.MmtfWriter;
+import edu.sdsc.mmtf.spark.mappers.StructureToBioassembly;
+import edu.sdsc.mmtf.spark.mappers.StructureToPolymerChains;
+import edu.sdsc.mmtf.spark.mappers.StructureToProteinDimers;
+
+/**
+ * Example demonstrating how to extract protein chains from
+ * PDB entries. This example uses a flatMapToPair function
+ * to transform a structure to its polymer chains.
+ * 
+ * @author Peter Rose
+ *
+ */
+public class MapToProteinDimers {
+
+	public static void main(String[] args) {
+
+	    SparkConf conf = new SparkConf().setMaster("local[*]").setAppName(CustomReportDemo.class.getSimpleName());
+	    JavaSparkContext sc = new JavaSparkContext(conf);
+	    List<String> pdbIds = Arrays.asList("1HV4"); // single protein chain
+	    JavaPairRDD<String, StructureDataInterface> pdb = MmtfReader.downloadMmtfFiles(pdbIds, sc).cache(); 
+	   
+	    pdb = pdb // read MMTF hadoop sequence file
+	    		.flatMapToPair(new StructureToProteinDimers());
+	    long count = pdb // read MMTF hadoop sequence file
+	    		.count();
+	    pdb = pdb.coalesce(1);
+	    System.out.println("# structures: " + count);
+	    MmtfWriter.writeMmtfFiles("D:/ProteinDimers", sc, pdb);
+	    
+	    sc.close();
+	}
+}
