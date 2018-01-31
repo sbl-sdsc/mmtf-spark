3a2d1229c08f8ab6d59a7b93e086dd1cebb4c490
Peter Rose <pwrose@ucsd.edu>
2018-01-30 23:22:42 -0800

    Updated documentation

--- src/main/java/edu/sdsc/mmtf/spark/io/demos/PdbToMmtfFull.java
+++ src/main/java/edu/sdsc/mmtf/spark/io/demos/PdbToMmtfFull.java
@@ -40,29 +40,20 @@ public class PdbToMmtfFull {
 	    
 	    // path to output directory
 	    String mmtfPath = args[1];
-	    
-	    long start = System.nanoTime();
-	    
+
 	    // instantiate Spark. Each Spark application needs these two lines of code.
-	    SparkConf conf = new SparkConf().setMaster("local[*]").setAppName(PdbToMmtfFull.class.getSimpleName());
+	    SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("PdbToMmtfFull");
 	    JavaSparkContext sc = new JavaSparkContext(conf);
 		 
-	    // read all PDB file recursively starting the specified directory
+	    // read PDB files recursively starting the specified directory
 	    JavaPairRDD<String, StructureDataInterface> structures = MmtfReader.readPdbFiles(pdbPath, sc);
 	    
-	    // save a MMTF-Hadoop Sequence File
+	    // save as an MMTF-Hadoop Sequence File
 	    MmtfWriter.writeSequenceFile(mmtfPath, sc, structures);
 	    
 	    System.out.println(structures.count() + " structures written to: " + mmtfPath);
 	    
-	    // check results
-	    MmtfReader.readSequenceFile(mmtfPath, sc)
-	    .foreach(t -> TraverseStructureHierarchy.printChainEntityGroupAtomInfo(t._2));
-	    
 	    // close Spark
-	    sc.close();
-	    
-	    long end = System.nanoTime();
-	    System.out.println((end-start)/1E9 + " sec."); 
+	    sc.close(); 
 	}
 }
