94a0071df1f66605d929919448cc8d10e494eb6c
pwrose <pwrose@ucsd.edu>
2018-02-06 10:21:41 -0800

    Removed SparkContext argument from JpredDataset. Removed JpredDemo.

--- src/main/java/edu/sdsc/mmtf/spark/datasets/JpredDataset.java
+++ src/main/java/edu/sdsc/mmtf/spark/datasets/JpredDataset.java
@@ -23,32 +23,35 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
 
 import edu.sdsc.mmtf.spark.ml.JavaRDDToDataset;
 
 /**
- * This class downloads the dataset used to train the
- * <a href="http://www.compbio.dundee.ac.uk/jpred/about_RETR_JNetv231_details.shtml">JPred 4/JNet (v.2.3.1)</a>
- * secondary structure predictor. It can be used as a reference
- * dataset for machine learning applications.
- * This dataset includes the ScopID, sequence, DSSP secondary
- * structure assignment, and a flag that indicates if data point
- * was part of the training set.
+ * This class downloads the dataset used to train the <a href=
+ * "http://www.compbio.dundee.ac.uk/jpred/about_RETR_JNetv231_details.shtml">JPred
+ * 4/JNet (v.2.3.1)</a> secondary structure predictor. It can be used as a
+ * reference dataset for machine learning applications. This dataset includes
+ * the ScopID, sequence, DSSP secondary structure assignment, and a flag that
+ * indicates if data point was part of the training set.
  * 
  * @author Yue Yu
  * @since 0.1.0
  * 
  */
 public class JpredDataset {
+	private static final String URL = "http://www.compbio.dundee.ac.uk/jpred/downloads/retr231.tar.gz";
+
 	/**
 	 * Gets JPred 4/JNet (v.2.3.1) secondary structure dataset.
-	 * @param sc JavaSparkContext
+	 * 
 	 * @return secondary structure dataset
-	 * @throws IOException if file cannot be downloaded or read
+	 * @throws IOException
+	 *             if file cannot be downloaded or read
 	 */
-	public static Dataset<Row> getDataset(JavaSparkContext sc) throws IOException {	
+	public static Dataset<Row> getDataset() throws IOException {
 		List<Row> res = new ArrayList<Row>();
-		String URL = "http://www.compbio.dundee.ac.uk/jpred/downloads/retr231.tar.gz";		
+
 		URL u = new URL(URL);
 		URLConnection conn = u.openConnection();
 		InputStream in = conn.getInputStream();
@@ -56,45 +59,46 @@ public class JpredDataset {
 		GzipCompressorInputStream gzIn = new GzipCompressorInputStream(fin);
 		TarArchiveInputStream tarIn = new TarArchiveInputStream(gzIn);
 		TarArchiveEntry entry = null;
-		
-		Set<String> scopIDs = new HashSet<>(); 
+
+		Set<String> scopIDs = new HashSet<>();
 		Map<String, String> sequences = new HashMap<String, String>();
 		Map<String, String> secondaryStructures = new HashMap<String, String>();
 		Map<String, String> trained = new HashMap<String, String>();
-		
+
 		while ((entry = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
 			if (entry.isDirectory()) {
 				continue;
 			}
 			BufferedReader br = new BufferedReader(new InputStreamReader(tarIn));
-			if(entry.getName().contains(".dssp"))
-			{
-				String scopID =  br.readLine().substring(1);
+			if (entry.getName().contains(".dssp")) {
+				String scopID = br.readLine().substring(1);
 				String secondaryStructure = br.readLine();
 				secondaryStructure = secondaryStructure.replace("-", "C");
 				secondaryStructures.put(scopID, secondaryStructure);
-			}
-			else if(entry.getName().contains(".fasta"))
-			{
-				String scopID =  br.readLine().substring(1);
+			} else if (entry.getName().contains(".fasta")) {
+				String scopID = br.readLine().substring(1);
 				String sequence = br.readLine();
 				scopIDs.add(scopID);
 				sequences.put(scopID, sequence);
-				if(entry.getName().contains("training/"))
+				if (entry.getName().contains("training/"))
 					trained.put(scopID, "true");
-				else if(entry.getName().contains("blind/"))
+				else if (entry.getName().contains("blind/"))
 					trained.put(scopID, "false");
 			}
 		}
 		tarIn.close();
-		
+
 		Iterator<String> iter = scopIDs.iterator();
 		while (iter.hasNext()) {
 			String scopID = iter.next();
-		    res.add(RowFactory.create(scopID, sequences.get(scopID), secondaryStructures.get(scopID),
-		    		trained.get(scopID)));
+			res.add(RowFactory.create(scopID, sequences.get(scopID), secondaryStructures.get(scopID),
+					trained.get(scopID)));
 		}
-		
+
+		SparkSession spark = SparkSession.builder().getOrCreate();
+		@SuppressWarnings("resource")
+		JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
+
 		JavaRDD<Row> data = sc.parallelize(res);
 		return JavaRDDToDataset.getDataset(data, "scopID", "sequence", "secondaryStructure", "trained");
 	}
