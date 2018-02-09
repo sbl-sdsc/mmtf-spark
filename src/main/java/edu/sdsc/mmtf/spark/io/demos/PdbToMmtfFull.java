20db92b8eb275d202264561fbc57388631012920
pwrose <pwrose@ucsd.edu>
2018-02-09 15:00:36 -0800

    Updated documentation

--- src/main/java/edu/sdsc/mmtf/spark/io/demos/PdbToMmtfFull.java
+++ src/main/java/edu/sdsc/mmtf/spark/io/demos/PdbToMmtfFull.java
@@ -25,14 +25,14 @@ public class PdbToMmtfFull {
      * Converts a directory containing PDB files into an MMTF-Hadoop Sequence file.
      * The input directory is traversed recursively to find PDB files.
      * 
-     * @param args args[0] <path-to-pdb_files>, args[1] <path-to-mmtf-hadoop-file>
+     * @param args args[0] <input-path-to-pdb_files>, args[1] <output-path-to-mmtf-hadoop-file>
      * 
      * @throws FileNotFoundException
      */
 	public static void main(String[] args) throws FileNotFoundException {  
 		
 	    if (args.length != 2) {
-	        System.out.println("Usage: PdbToMmtfFull <path-to-pdb_files> <path-to-mmtf-hadoop-file>");
+	        System.out.println("Usage: PdbToMmtfFull <input-path-to-pdb_files> <output-path-to-mmtf-hadoop-file>");
 	    }
 	    
 	    // path to input directory
@@ -45,7 +45,7 @@ public class PdbToMmtfFull {
 	    SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("PdbToMmtfFull");
 	    JavaSparkContext sc = new JavaSparkContext(conf);
 		 
-	    // read PDB files recursively starting the specified directory
+	    // read PDB files recursively starting from the specified top level directory
 	    JavaPairRDD<String, StructureDataInterface> structures = MmtfReader.readPdbFiles(pdbPath, sc);
 	    
 	    // save as an MMTF-Hadoop Sequence File
